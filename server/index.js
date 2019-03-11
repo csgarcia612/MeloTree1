3f475f81cf2b5040a0c0a16bd9846b37b416d1bf
csgarcia612 <csgarcia612@comcast.net>
2019-03-10 19:09:03 -0600

    hosting site

--- server/index.js
+++ server/index.js
@@ -1,49 +1,49 @@
 const express = require('express'),
-	bodyParser = require('body-parser'),
-	session = require('express-session'),
-	massive = require('massive'),
-	app = express(),
-	RedisStore = require('connect-redis')(session),
-	cors = require('cors'),
-	graphqlHTTP = require('express-graphql'),
-	gqlConfigs = require('./graphql/graphqlConfigs'),
-	auth0Controller = require('./controllers/auth0Controller'),
-	nodemailerController = require('./controllers/nodemailerController'),
-	usersController = require('./controllers/usersController'),
-	stripeController = require('./controllers/stripeController');
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  massive = require('massive'),
+  app = express(),
+  RedisStore = require('connect-redis')(session),
+  cors = require('cors'),
+  graphqlHTTP = require('express-graphql'),
+  gqlConfigs = require('./graphql/graphqlConfigs'),
+  auth0Controller = require('./controllers/auth0Controller'),
+  nodemailerController = require('./controllers/nodemailerController'),
+  usersController = require('./controllers/usersController'),
+  stripeController = require('./controllers/stripeController');
 
 require('dotenv').config();
 
 app.use(bodyParser.json());
 
 massive(process.env.CONNECTION_STRING)
-	.then(db => {
-		exports.database = db;
-		console.log('Database Connection : ONLINE');
-	})
-	.catch(error =>
-		console.log(('😡 Error with massive DB connection 😡', error))
-	);
+  .then(db => {
+    exports.database = db;
+    console.log('Database Connection : ONLINE');
+  })
+  .catch(error =>
+    console.log(('😡 Error with massive DB connection 😡', error))
+  );
 
 app.use(cors());
 
 app.use(
-	'/graphiql',
-	graphqlHTTP({
-		schema: gqlConfigs.schema,
-		rootValue: gqlConfigs.root,
-		graphiql: true
-	})
+  '/graphiql',
+  graphqlHTTP({
+    schema: gqlConfigs.schema,
+    rootValue: gqlConfigs.root,
+    graphiql: true
+  })
 );
 
 app.use(
-	session({
-		store: new RedisStore({ url: process.env.REDIS_URI }),
-		secret: process.env.SESSION_SECRET,
-		saveUninitialized: false,
-		resave: false,
-		cookie: { maxAge: 1000 * 60 * 60 * 24 * 2 }
-	})
+  session({
+    store: new RedisStore({ url: process.env.REDIS_URI }),
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 2 }
+  })
 );
 
 app.get('/api/user-data', usersController.getUser);
@@ -56,7 +56,14 @@ app.post('/api/nodemailer', nodemailerController.send);
 
 app.post('/api/stripe', stripeController.creditCharge);
 
+app.use(express.static(`${__dirname}/../build`));
+
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 const PORT = 4000;
 app.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
